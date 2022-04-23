8a54bcb01eb96c30fde3c00bc8e697132a982752
Yu Hien <nyhdid@gmail.com>
2022-04-23 22:44:04 +0800

    Improve payment cancel page, added link renavigate to home

--- src/components/Payment/Cancel.jsx
+++ src/components/Payment/Cancel.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Cancel = () => {
     return (
-        <div>
-            <h1>Payment Cancelled</h1>
+        <div className="d-flex justify-content-center">
+            <div className="mt-5">
+                <h1>Payment Cancelled</h1>
+            </div>
+            <p>
+                <Link to="/main">
+                    Back to Home
+                </Link>    
+            </p>
+
         </div>
     )
 }
