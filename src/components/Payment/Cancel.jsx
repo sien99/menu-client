52b17210513084adce43976f129a459c3bd94e25
Yu Hien <nyhdid@gmail.com>
2022-04-23 22:50:53 +0800

    Improve payment cancel page

--- src/components/Payment/Cancel.jsx
+++ src/components/Payment/Cancel.jsx
@@ -5,13 +5,17 @@ const Cancel = () => {
     return (
         <div className="d-flex justify-content-center">
             <div className="mt-5">
-                <h1>Payment Cancelled</h1>
+                <h1>
+                    Payment Cancelled
+                
+                </h1>
+                <p>
+                    <Link to="/main">
+                        Back to Home
+                    </Link>    
+                </p>
             </div>
-            <p>
-                <Link to="/main">
-                    Back to Home
-                </Link>    
-            </p>
+
 
         </div>
     )
